39d49dcb57be02755db2d070900a89d39342a4d5
github username <github email address>
2018-02-20 22:05:43 -0500

    scraped actor name, movie title, and movie url

--- scrape.js
+++ scrape.js
@@ -6,26 +6,27 @@ let url = 'https://www.imdb.com/search/name?birth_monthday=02-02'
 request(url, function (error, response, html) {
   if (!error && response.statusCode == 200) {
     let $ = cheerio.load(html);
-    ignoreWhitespace: false
+
     $('.lister-item.mode-detail').map(function(i, element){
-      let actorName = $(this).find('h3').text();
+      let actorName = $(this).find('h3 a').text().trim()
       let photoURL = $(this).find('img').attr('src')
       let profileURL = $(this).find('a').attr('href');
-      let works = $('p.text-muted.text-small a').text()
+      let movieTitle = $(this).find('.text-muted.text-small a').text().trim()
+      let movieURL = $(this).find('.text-muted.text-small a').attr('href');
 
-      console.log(profileURL)
+      console.log(movieURL)
         let people = {
-          name: 'Gemma Arterton',
+          name: actorName,
           photoUrl: photoURL,
           profileURL: profileURL,
           mostKnownWork: {
-            title: works,
-            url: 'Prince of Persia: The Sands of Time',
-            rating: 6.6,
-            director: ""
+            title: movieTitle,
+            url: movieURL,
+            rating: 0,
+            director:''
           }
         };
-            // console.log(people)
+        console.log(people)
     });
   }
 });
