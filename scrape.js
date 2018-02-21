b742dc6423ccd52c52721138c6ab2b934fe627b8
github username <github email address>
2018-02-20 21:32:54 -0500

    scraped photoURL and profileURL sucessfully

--- scrape.js
+++ scrape.js
@@ -1,12 +1,31 @@
 let request = require('request');
+let fs = require('fs')
 let cheerio = require('cheerio');
+let url = 'https://www.imdb.com/search/name?birth_monthday=02-02'
 
-request('https://www.imdb.com/search/name?birth_monthday=02-02', function (error, response, html) {
+request(url, function (error, response, html) {
   if (!error && response.statusCode == 200) {
-    var $ = cheerio.load(html);
-    $('.lister-item.mode-detail').each(function(i, element){
-      var actorName = $('.lister-item-header a').text();
-      console.log(actorName);
+    let $ = cheerio.load(html);
+    ignoreWhitespace: false
+    $('.lister-item.mode-detail').map(function(i, element){
+      let actorName = $(this).find('h3').text();
+      let photoURL = $(this).find('img').attr('src')
+      let profileURL = $(this).find('a').attr('href');
+      let works = $('p.text-muted.text-small a').text()
+
+      console.log(profileURL)
+        let people = {
+          name: 'Gemma Arterton',
+          photoUrl: photoURL,
+          profileURL: profileURL,
+          mostKnownWork: {
+            title: works,
+            url: 'Prince of Persia: The Sands of Time',
+            rating: 6.6,
+            director: ""
+          }
+        };
+            // console.log(people)
     });
   }
 });
