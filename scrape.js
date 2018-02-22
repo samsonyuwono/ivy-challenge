1809af8460efdd5320efcc3272edeede5d08a894
github username <github email address>
2018-02-21 20:23:53 -0500

    scrape for movieRating and movieDirector working

--- scrape.js
+++ scrape.js
@@ -1,14 +1,19 @@
 let request = require('request');
+let rp = require('request-promise');
 let fs = require('fs')
 let cheerio = require('cheerio');
 let url = 'https://www.imdb.com/search/name?birth_monthday='
 let pageNumber; //function to increment by 50 page number by 50
-function getBirthDayPeople(birthMonth, birthDay){
+let people = []
+//out = { people: people }
+//get people array out of it
+//make another function that loops through for each person's url
+//after get birthday people run console.log(people)
+function getBirthDayPeople(birthMonth, birthDay, positionStart){
 
-  request(url + birthMonth + '-' + birthDay, function (error, response, html) {
+  request(url + birthMonth + '-' + birthDay + '&start=' + positionStart, function (error, response, html) {
     if (!error && response.statusCode == 200) {
       let $ = cheerio.load(html);
-      let people = []
       let imdb = 'http://www.imdb.com'
       $('.lister-item.mode-detail').each(function(i, element){
         let actorName = $(this).find('h3 a').text().trim()
@@ -27,26 +32,52 @@ function getBirthDayPeople(birthMonth, birthDay){
             title: movieTitle,
             url: movieURL,
             rating: 0,
-            director:''
+            director: ''
           }
         };
         people.push(results)
       });
-      console.log(people)
+
+      people.forEach(function(element){
+  let movieURL2= element.mostKnownWork.url
+
+  request(movieURL2, function (error, response, html) {
+    if(!error && response.statusCode == 200){
+      var $ = cheerio.load(html);
+
+      $('#title-overview-widget').each(function(i, element){
+        let movieRating = $(this).find('.imdbRating').children().children().first().text();
+        let movieDirector = $(this).find('.credit_summary_item').children().first().next().text().trim();
+        console.log(movieRating)
+        //open movie url link and scrape from there
+      })
     }
   });
+  // console.log(movieURL2)
+})
+
+      // console.log(people)
+    }
+  })
 }
+//people = getBirthDayPeople
 getBirthDayPeople(02, 02)
+
+// function getMovieInfo(){
 //
-// request('http://www.imdb.com/title/tt0325980/?ref_=nm_knf_i1', function (error, response, html) {
-//   if(!error){
-//     var $ = cheerio.load(html);
+//   request('http://www.imdb.com/title/tt0089243/', function (error, response, html) {
+//     if(!error){
+//       var $ = cheerio.load(html);
 //
-//     $('.plot_summary_wrapper').each(function(i, element){
-//       let movieRating = $(this).find('.').text();
-//       let movieDirector = $(this).find('.plot_summary .inline span').text();
-//       //open movie url link and scrape from there
-//       console.log(movieDirector)
-//     })
-//   }
-// });
+//       $('#title-overview-widget').each(function(i, element){
+//         // console.log(this)
+//         let movieDirector = $(this).find('.credit_summary_item').children().first().next().text().trim();
+//         let movieRating = $(this).find('.imdbRating').children().children().first().text();
+//         console.log(movieRating)
+//         //open movie url link and scrape from there
+//       })
+//     }
+//   });
+// }
+//
+// getMovieInfo()
