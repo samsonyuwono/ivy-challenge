2e5c56f098a30316c23afbaadfb519ea11320812
github username <github email address>
2018-02-21 23:25:40 -0500

    one more requirement

--- scrape.js
+++ scrape.js
@@ -5,14 +5,15 @@ let cheerio = require('cheerio');
 let url = 'https://www.imdb.com/search/name?birth_monthday='
 let pageNumber; //function to increment by 50 page number by 50
 let people = []
+let movieInfo = []
 //out = { people: people }
 //get people array out of it
 //make another function that loops through for each person's url
 //after get birthday people run console.log(people)
 function getBirthDayPeople(birthMonth, birthDay, positionStart){
-
   request(url + birthMonth + '-' + birthDay + '&start=' + positionStart, function (error, response, html) {
     if (!error && response.statusCode == 200) {
+
       let $ = cheerio.load(html);
       let imdb = 'http://www.imdb.com'
       $('.lister-item.mode-detail').each(function(i, element){
@@ -21,9 +22,7 @@ function getBirthDayPeople(birthMonth, birthDay, positionStart){
         let profileURL = imdb + $(this).find('a').attr('href');
         let movieTitle = $(this).find('.text-muted.text-small a').text().trim()
         let movieURL = imdb + $(this).find('.text-muted.text-small a').attr('href');
-        // let movieRating = $(this).find('.star-box-giga-star').text();
-        // let movieDirector = $(this).find('.credit_summary_item .inline').text();
-        //open movie url link and scrape from there
+
         let results = {
           name: actorName,
           photoUrl: photoURL,
@@ -37,47 +36,28 @@ function getBirthDayPeople(birthMonth, birthDay, positionStart){
         };
         people.push(results)
       });
-
       people.forEach(function(element){
-  let movieURL2= element.mostKnownWork.url
-
-  request(movieURL2, function (error, response, html) {
-    if(!error && response.statusCode == 200){
-      var $ = cheerio.load(html);
-
-      $('#title-overview-widget').each(function(i, element){
-        let movieRating = $(this).find('.imdbRating').children().children().first().text();
-        let movieDirector = $(this).find('.credit_summary_item').children().first().next().text().trim();
-        console.log(movieRating)
-        //open movie url link and scrape from there
+        let movieURLInfo= element.mostKnownWork.url
+        // console.log(element)
+        request(movieURLInfo, function (error, response, html) {
+          if(!error && response.statusCode == 200){
+            var $ = cheerio.load(html);
+            $('#title-overview-widget').each(function(i, element){
+              let movieRating = $(this).find('.imdbRating').children().children().first().text();
+              let movieDirector = $(this).find('.credit_summary_item').children().first().next().text().trim().replace(/,/g, '');
+              //open movie url link and scrape from there
+              let movieResults = {
+                rating: movieRating,
+                director: movieDirector
+              }
+              movieInfo.push(movieResults)
+            })
+          }
+        });
+          console.log(movieInfo)
       })
-    }
-  });
-  // console.log(movieURL2)
-})
-
-      // console.log(people)
     }
   })
 }
 //people = getBirthDayPeople
 getBirthDayPeople(02, 02)
-
-// function getMovieInfo(){
-//
-//   request('http://www.imdb.com/title/tt0089243/', function (error, response, html) {
-//     if(!error){
-//       var $ = cheerio.load(html);
-//
-//       $('#title-overview-widget').each(function(i, element){
-//         // console.log(this)
-//         let movieDirector = $(this).find('.credit_summary_item').children().first().next().text().trim();
-//         let movieRating = $(this).find('.imdbRating').children().children().first().text();
-//         console.log(movieRating)
-//         //open movie url link and scrape from there
-//       })
-//     }
-//   });
-// }
-//
-// getMovieInfo()
